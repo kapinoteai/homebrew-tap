5c4fd4510308cc9956f5eef72008436cfa443afb
guangzhengli <iguangzhengli@gmail.com>
2026-08-21 21:32:48 +0800

    Add kapinote cask served from dl.kapinote.com

---
+++ Casks/kapinote.rb
@@ -0,0 +1,28 @@
+cask "kapinote" do
+  version "0.9.13"
+  sha256 "577d8739ed10eb1bdf115c8117fb19f396649941e75ffe18591693cf6a08a915"
+
+  url "https://dl.kapinote.com/v#{version}/Kapinote_#{version}_aarch64.dmg"
+  name "Kapinote"
+  desc "Desktop meetings app"
+  homepage "https://kapinote.com/"
+
+  livecheck do
+    url "https://dl.kapinote.com/latest.json"
+    strategy :json do |json|
+      json["version"]
+    end
+  end
+
+  auto_updates true
+  depends_on macos: :sonoma
+  depends_on arch: :arm64
+
+  app "Kapinote.app"
+
+  zap trash: [
+    "~/Library/Application Support/Kapinote",
+    "~/Library/Caches/com.guangzhengli.kapinote",
+    "~/Library/Preferences/com.guangzhengli.kapinote.plist",
+  ]
+end
