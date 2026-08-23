c6a6298b846a6a57605cba7e6b68fd81fe7918f7
guangzhengli <iguangzhengli@gmail.com>
2026-08-24 01:11:37 +0800

    Update kapinote to v0.10.0

--- Casks/kapinote.rb
+++ Casks/kapinote.rb
@@ -1,6 +1,6 @@
 cask "kapinote" do
-  version "0.9.15"
-  sha256 "4ced75c24baf2510a2e230cd2ad40d928fc7a2876a8dae3fe61dd4e02da73102"
+  version "0.10.0"
+  sha256 "8bf9ee0859a731925aee1180f9698e460c68178788f1df0f436acb8e393e0abb"
 
   url "https://dl.kapinote.com/v#{version}/Kapinote_#{version}_aarch64.dmg"
   name "Kapinote"
